8c2f1addd9d2162adb1666995fcef0f957c0de79
Dharma Raj Thapa <relizon44@gmail.com>
2016-08-11 15:36:55 +0545

    assets path fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,7 @@
 
     <!-- Styles -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
-    <link href="{!!Html::style('css/app.css') !!}" rel="stylesheet">
+    <link href="{{ URL::asset('public/css/app.css') }}" rel="stylesheet">
 
     <style>
         body {
@@ -80,7 +80,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.7/angular.min.js"></script>
-    <script src="{!!Html::script('js/main.js') !!}"></script>
+    <script src="{{ URL::asset('public/js/main.js') }}"></script>
     {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
 </body>
 </html>
